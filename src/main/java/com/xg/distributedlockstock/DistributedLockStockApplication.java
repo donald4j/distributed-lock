628200222fd40415fa42edd63a76a9f81157adf5
HuXueping <13224010600@163.com>
2024-06-24 10:10:15 +0800

    dev:init-初始化业务-配置DataSource

---
+++ src/main/java/com/xg/distributedlockstock/DistributedLockStockApplication.java
@@ -0,0 +1,13 @@
+package com.xg.distributedlockstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedLockStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedLockStockApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xg/distributedlockstock/DistributedLockStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xg.distributedlockstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedLockStockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
